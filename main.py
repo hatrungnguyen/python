585adfd2fb60a6515c8a8f2d2bad05c97b7a5f46
hatrungnguyen <hatrungnguyen04@gmail.com>
2023-02-22 20:35:39 +0700

    section 5

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 listoftouristattraction = []
 while True:
-    x = input("add list,show list,edit or exit:")
+    x = input("Let's go to experience!")
     match x:
         case 'add' | 'add list':
             y = input("new location:")
             listoftouristattraction.append(y)
         case 'show' | 'show list':
-            for item in listoftouristattraction:
-                print(item)
+            for item, index in enumerate(listoftouristattraction):
+                r = f"{item + 1}.{index}"
+                print(r)
         case 'exit':
             break
         case 'edit':
             z = int(input("what place you want to change?"))
             z = z-1
             listoftouristattraction[z] = input("new location:")
-
+        case 'complete':
+            z = int(input("what place you want to complete?"))
+            z = z - 1
+            listoftouristattraction.pop(z)
 print("thank for using")
