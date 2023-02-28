2f7c12af64c26c4167ca90390b16c7c4e2e2bbd5
hatrungnguyen <hatrungnguyen04@gmail.com>
2023-02-28 21:30:16 +0700

    section 5

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
-x = input("what your name?")
-tên = x.capitalize()
-print(tên)
+
+while True:
+    x = input("Let's go to experience!")
+    match x:
+        case 'add' | 'add list':
+            y = input("new location:") + "\n"
+
+            with open('listoftouristattraction.txt', 'r') as file:
+                listoftouristattraction = file.readlines()
+
+            listoftouristattraction.append(y)
+
+            with open('listoftouristattraction.txt', 'w') as file:
+                file.writelines(listoftouristattraction)
+
+        case 'show' | 'show list':
+
+
+            with open('listoftouristattraction.txt', 'r') as file:
+                listoftouristattraction = file.readlines()
+            # a = [item.strip('listoftouristattraction.txt', 'r')]
+            for item, index in enumerate(listoftouristattraction):
+                index = index.strip('\n')
+                r = f"{item + 1}.{index}"
+                print(r)
+        case 'exit':
+            break
+        case 'edit':
+            z = int(input("what place you want to change?"))
+            with open('listoftouristattraction.txt', 'r') as file:
+                listoftouristattraction = file.readlines()
+
+            z = z-1
+            listoftouristattraction[z] = input("new location:") + "\n"
+            with open('listoftouristattraction.txt', 'w') as file:
+                file.writelines(listoftouristattraction)
+
+        case 'complete':
+            z = int(input("what place you want to complete?"))
+            with open('listoftouristattraction.txt', 'r') as file:
+                listoftouristattraction = file.readlines()
+
+            z = z - 1
+            placeremove = listoftouristattraction[z].strip('\n')
+            listoftouristattraction.pop(z)
+            with open('listoftouristattraction.txt', 'w') as file:
+                file.writelines(listoftouristattraction)
+            message = f"{placeremove} has been deleted"
+            print(message)
+print("thank for using")
